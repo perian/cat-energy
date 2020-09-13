4b39a7b7fc7f23a28905b58ae3a93e0bd00d23ec
perian <dmperian@gmail.com>
2020-09-13 22:43:16 +0300

    При работающем JS меню изначально скрыто

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,8 +1,29 @@
+// var openCloseButton = document.querySelector(".main-nav__toggle");
+// var mainMenu = document.querySelector(".main-nav");
+//
+// if (mainMenu.classList.contains("main-nav--nojs")) {
+//   mainMenu.classList.remove("main-nav--nojs");
+// }
+//
+// if (mainMenu.classList.contains("main-nav--opened")) {
+//   mainMenu.classList.remove("main-nav--opened");
+//   mainMenu.classList.toggle("main-nav--closed");
+// }
+//
+// openCloseButton.onclick = function () {
+//   mainMenu.classList.toggle("main-nav--opened");
+//   mainMenu.classList.toggle("main-nav--closed");
+// }
+
+
 var openCloseButton = document.querySelector(".main-nav__toggle");
 var mainMenu = document.querySelector(".main-nav");
 
-if (mainMenu.classList.contains("main-nav--nojs")) {
-  mainMenu.classList.remove("main-nav--nojs");
+mainMenu.classList.add("main-nav--js");
+
+if (mainMenu.classList.contains("main-nav--opened")) {
+  mainMenu.classList.remove("main-nav--opened");
+  mainMenu.classList.add("main-nav--closed");
 }
 
 openCloseButton.onclick = function () {
