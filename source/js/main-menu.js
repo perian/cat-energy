f83c9567d726e0cb652091557924db6866701990
perian <dmperian@gmail.com>
2020-09-15 17:33:58 +0300

    Проверяет, что обьет не пуст

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,25 +1,9 @@
-// var openCloseButton = document.querySelector(".main-nav__toggle");
-// var mainMenu = document.querySelector(".main-nav");
-//
-// if (mainMenu.classList.contains("main-nav--nojs")) {
-//   mainMenu.classList.remove("main-nav--nojs");
-// }
-//
-// if (mainMenu.classList.contains("main-nav--opened")) {
-//   mainMenu.classList.remove("main-nav--opened");
-//   mainMenu.classList.toggle("main-nav--closed");
-// }
-//
-// openCloseButton.onclick = function () {
-//   mainMenu.classList.toggle("main-nav--opened");
-//   mainMenu.classList.toggle("main-nav--closed");
-// }
-
-
 var openCloseButton = document.querySelector(".main-nav__toggle");
 var mainMenu = document.querySelector(".main-nav");
 
-mainMenu.classList.add("main-nav--js");
+if (mainMenu !== null ) {
+  mainMenu.classList.add("main-nav--js");
+}
 
 if (mainMenu.classList.contains("main-nav--opened")) {
   mainMenu.classList.remove("main-nav--opened");
@@ -29,4 +13,4 @@ if (mainMenu.classList.contains("main-nav--opened")) {
 openCloseButton.onclick = function () {
   mainMenu.classList.toggle("main-nav--opened");
   mainMenu.classList.toggle("main-nav--closed");
-}
+};
